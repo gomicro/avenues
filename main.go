cf630483484f4a0c8f08128ca95e8446854af184
Daniel Hess <dan9186@gmail.com>
2018-01-01 02:02:47 -0800

    adding initial POC bits

---
+++ main.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+
+	log "github.com/gomicro/ledger"
+
+	"gopkg.in/yaml.v2"
+)
+
+type configuration struct {
+	Services map[string]string `yaml:"services"`
+	Routes   map[string]string `yaml:"routes"`
+}
+
+var (
+	client *http.Client
+	config configuration
+)
+
+func init() {
+	b, err := ioutil.ReadFile("./routes.yaml")
+	if err != nil {
+		log.Errorf("Failed to read config file: %v", err.Error())
+		os.Exit(1)
+	}
+
+	err = yaml.Unmarshal(b, &config)
+	if err != nil {
+		log.Errorf("Failed to unmarshal config file: %v", err.Error())
+		os.Exit(1)
+	}
+
+	client = &http.Client{
+		Transport: &http.Transport{
+			MaxIdleConnsPerHost: 50,
+			MaxIdleConns:        50,
+		},
+	}
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		u, err := proxyURL(req.URL)
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			w.Write([]byte("not found"))
+			return
+		}
+
+		if req.Body != nil {
+			defer req.Body.Close()
+		}
+
+		//TODO: stream instead of read all?
+		reqBody, err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		proxyReq, err := http.NewRequest(req.Method, u.String(), bytes.NewBuffer(reqBody))
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		proxyReq.Header = req.Header
+
+		resp, err := client.Do(proxyReq)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		if resp.Body != nil {
+			defer resp.Body.Close()
+		}
+
+		//TODO: stream instead of read all?
+		b, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		for k, v := range resp.Header {
+			w.Header().Set(k, strings.Join(v, ""))
+		}
+
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "*")
+		w.Header().Set("Cache-Control", "no-store, no-cache, must-revalidate, post-check=0, pre-check=0")
+		w.Header().Set("Vary", "Accept-Encoding")
+
+		w.WriteHeader(resp.StatusCode)
+		w.Write(b)
+	})
+
+	log.Infof("Listening on %v:%v", "0.0.0.0", "8000")
+	http.ListenAndServe("0.0.0.0:8000", mux)
+}
+
+func proxyURL(reqURL *url.URL) (*url.URL, error) {
+	serviceName, ok := pathToServiceName(reqURL.Path)
+	if !ok {
+		return nil, fmt.Errorf("not found")
+	}
+
+	serviceAddress, ok := config.Services[serviceName]
+	if !ok {
+		return nil, fmt.Errorf("not found")
+	}
+
+	u, err := url.Parse(serviceAddress)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse service address")
+	}
+
+	u.Path = reqURL.Path
+	u.RawQuery = reqURL.Query().Encode()
+
+	return u, nil
+}
+
+func pathToServiceName(path string) (string, bool) {
+	for route, serviceName := range config.Routes {
+		if strings.HasPrefix(trailingSlash(path), route) {
+			return serviceName, true
+		}
+	}
+
+	return "", false
+}
+
+func trailingSlash(path string) string {
+	if !strings.HasSuffix(path, "/") {
+		return fmt.Sprintf("%v/", path)
+	}
+
+	return path
+}
