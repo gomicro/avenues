ba2caf36b0bac45e89846740e07e6242d76deefd
Daniel Hess <dan9186@gmail.com>
2018-10-30 10:47:30 -0700

    fixing the tests

--- config/config_test.go
+++ config/config_test.go
@@ -18,13 +18,9 @@ func TestConfig(t *testing.T) {
 			c, err := config.ParseFromFile()
 			Expect(err).To(BeNil())
 
-			Expect(len(c.Services)).To(Equal(2))
-			Expect(c.Services["foo"]).To(Equal("http://foo:4567"))
-			Expect(c.Services["bar"]).To(Equal("http://bar:4567"))
-
-			Expect(len(c.Routes)).To(Equal(2))
-			Expect(c.Routes["/v1/foo"]).To(Equal("foo"))
-			Expect(c.Routes["/v1/bar"]).To(Equal("bar"))
+			Expect(len(c.Routes)).To(Equal(4))
+			Expect(c.Routes["/v1/foo"].Backend).To(Equal("http://foo:4567"))
+			Expect(c.Routes["/v1/bar"].Backend).To(Equal("http://foo:4567"))
 		})
 
 		g.It("should parse a custom config file set in the environment", func() {
@@ -33,11 +29,8 @@ func TestConfig(t *testing.T) {
 			c, err := config.ParseFromFile()
 			Expect(err).To(BeNil())
 
-			Expect(len(c.Services)).To(Equal(1))
-			Expect(c.Services["baz"]).To(Equal("http://baz:4567"))
-
 			Expect(len(c.Routes)).To(Equal(1))
-			Expect(c.Routes["/v1/baz"]).To(Equal("baz"))
+			Expect(c.Routes["/v1/baz"].Backend).To(Equal("http://baz:4567"))
 		})
 
 		g.It("should return an error when it can't read the file", func() {
